ab211aa11e8dbf06b3ca9c0d32bf0f64ed15fb95
Ganda Wenang Sani <Wenangsani@users.noreply.github.com>
2020-04-20 08:20:54 -0700

    aa

--- functions/index.php
+++ functions/index.php
@@ -1,3 +1,13 @@
 <?php
    require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+
+// Instantiate an Amazon S3 client.
+$s3 = new S3Client([
+    'version' => 'latest',
+    'region'  => 'us-west-2'
+]);
+
+print_r($s3);
 ?>
